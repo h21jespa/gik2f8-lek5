8cee6d3766d77836af28b5c02fe6e05d915050da
Jesper Andersson <h21jespa@du.se>
2022-12-20 12:36:07 +0100

    Quickfix

--- 02-todo/client/Api.js
+++ 02-todo/client/Api.js
@@ -1,31 +1,30 @@
 class Api {
-    url = '';
+  url = "";
 
-    constructor(url) {
-        this.url = url;
-    }
-    /* Create = POST*/
-    create(data){
+  constructor(url) {
+    this.url = url;
+  }
+  /* Create = POST*/
+  create(data) {
+    const JSONData = JSON.stringify(data);
+    console.log(`Sending ${JSONData} to ${this.url}`);
 
-        const JSONData = JSON.stringify(data);
-        console.log(`Sending ${JSONData} to ${this.url}`);
+    const request = new Request(this.url, {
+      method: "POST",
+      body: JSONData,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
 
-        const request = new Request(this.url, {
-            method: 'POST',
-            body: JSONData,
-            headers:{
-                'content-type': 'application/json'
-            }
-        });
+    console.log(request);
 
-        console.log(request);
+    fetch(request)
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+  }
 
-        fetch(request)
-        .then((result) => result.json())
-        .then((data) => console.log(data));
-    }
+  getAll() {}
 
-    getAll(){}
-
-    removeEventListener(){}
+  removeEventListener() {}
 }
--- 02-todo/client/script.js
+++ 02-todo/client/script.js
@@ -11,64 +11,60 @@ let titleValid = true;
 let descriptionValid = true;
 let dueDateValid = true;
 
-const api = new Api('http://localhost:5000/tasks');
+const api = new Api("http://localhost:5000/tasks");
 
 function validateField(field) {
-    const {name, value} = field;
-    let = validationMessage = "";
-    switch(name) {
-        case "title":{
-            if (value.length < 2){
-                titleValid = false;
-                validationMessage = "Fältet 'Titel' måste innehålla minst 2 tecken"
-            }
-                else if (value.length > 100) {
-                titleValid = false
-                validationMessage = "Fältet 'Titel' får inte innehålla mer än 100 tecken"
-            }
-            break;
-        }
-        
-        case "description":{
-            if (value.length > 500){
-                descriptionValid = false;
-                validationMessage = "Fältet 'beskrivning' får inte innehålla mer än 100 tecken"
-            }
-            break;
-        }
-        case "dueDate":{
-            if (value.length == 0){
-                descriptionValid = false;
-                validationMessage = "Fältet 'Slutförd senast' är obligatorisk"
-            }
-            break;
-        } 
+  const { name, value } = field;
+  let = validationMessage = "";
+  switch (name) {
+    case "title": {
+      if (value.length < 2) {
+        titleValid = false;
+        validationMessage = "Fältet 'Titel' måste innehålla minst 2 tecken";
+      } else if (value.length > 100) {
+        titleValid = false;
+        validationMessage =
+          "Fältet 'Titel' får inte innehålla mer än 100 tecken";
+      }
+      break;
     }
-    field.previousElementSibling.innerText = validationMessage;
-    field.previousElementSibling.classList.remove('hidden');
 
- }
- 
- 
+    case "description": {
+      if (value.length > 500) {
+        descriptionValid = false;
+        validationMessage =
+          "Fältet 'beskrivning' får inte innehålla mer än 100 tecken";
+      }
+      break;
+    }
+    case "dueDate": {
+      if (value.length == 0) {
+        descriptionValid = false;
+        validationMessage = "Fältet 'Slutförd senast' är obligatorisk";
+      }
+      break;
+    }
+  }
+  field.previousElementSibling.innerText = validationMessage;
+  field.previousElementSibling.classList.remove("hidden");
+}
+
 function onSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if(titleValid && descriptionValid && dueDateValid) {
-        console.log('submit');
-        saveTask();
-    }
+  if (titleValid && descriptionValid && dueDateValid) {
+    console.log("submit");
+    saveTask();
+  }
 
-    
-    function saveTask() {
-        const task = {
-            title: todoForm.title.value,
-            description: todoForm.description.value,
-            dueDate: todoForm.dueDate.value,
-            completed: false
-        };
-        
-        api.create(task);
+  function saveTask() {
+    const task = {
+      title: todoForm.title.value,
+      description: todoForm.description.value,
+      dueDate: todoForm.dueDate.value,
+      completed: false,
+    };
 
-
-    }
+    api.create(task);
+  }
 }
